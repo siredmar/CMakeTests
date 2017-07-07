1ca1b9121e7742cf3a4cbefa96bd257a9daafa64
Armin Schlegel <armin.schlegel@gmx.de>
2017-07-07 10:28:40 +0200

    Added basic testing functionality

--- Part1/src/tutorial.cxx
+++ Part1/src/tutorial.cxx
@@ -1,12 +1,15 @@
 // A simple program that computes the square root of a number
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include "TutorialConfig.h"
 #ifdef USE_MYMATH
 #include "MathFunctions/MathFunctions.h"
 #endif
 #include "Test/test.h"
+
+static int StringIsNumber(char * str);
 
 int main (int argc, char *argv[])
 {
@@ -20,7 +23,14 @@ int main (int argc, char *argv[])
     fprintf(stdout,"Usage: %s number\n",argv[0]);
     return 1;
     }
+
   double inputValue = atof(argv[1]);
+
+  if(StringIsNumber(argv[1]) == 0)
+  {
+      fprintf(stdout,"Input must be an integer number!\n");
+      return -1;
+  }
 
 test();
 
@@ -35,4 +45,19 @@ test();
   fprintf(stdout,"The square root of %g is %g\n",
           inputValue, outputValue);
   return 0;
+}
+
+static int StringIsNumber(char * str)
+{
+    int ret = 1;
+    int cnt;
+    for(cnt = 0; cnt < strlen(str); cnt++)
+    {
+        if(!(str[cnt] >= '0' && str[cnt] <= '9' || str[cnt] == '.' || str[cnt] == '-'))
+        {
+            ret = 0;
+            break;
+        }
+    }
+    return ret;
 }
